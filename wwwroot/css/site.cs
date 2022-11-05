b61ee6457984c4349eb5d0c8eeb07c5e1db1e542
AmauriLuiz <108699997+AmauriLuiz@users.noreply.github.com>
2022-11-05 20:49:13 -0300

    Add files via upload

---
+++ wwwroot/css/site.cs
@@ -0,0 +1,168 @@
+/* Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+for details on configuring this project to bundle and minify static web assets. */
+
+a.navbar-brand {
+    white-space: normal;
+    text-align: center;
+    word-break: break-all;
+}
+
+/* Provide sufficient contrast against white background */
+a {
+    color: #0366d6;
+}
+
+.btn-primary {
+    color: #fff;
+    background-color: #1b6ec2;
+    border-color: #1861ac;
+}
+
+.nav-pills .nav-link.active, .nav-pills .show > .nav-link {
+    color: #fff;
+    background-color: #1b6ec2;
+    border-color: #1861ac;
+}
+
+/* Sticky footer styles
+-------------------------------------------------- */
+html {
+    font-size: 14px;
+}
+
+@media (min-width: 768px) {
+    html {
+        font-size: 16px;
+    }
+}
+
+.border-top {
+    border-top: 1px solid #e5e5e5;
+}
+
+.border-bottom {
+    border-bottom: 1px solid #e5e5e5;
+}
+
+.box-shadow {
+    box-shadow: 0 .25rem .75rem rgba(0, 0, 0, .05);
+}
+
+button.accept-policy {
+    font-size: 1rem;
+    line-height: inherit;
+}
+
+/* Sticky footer styles
+-------------------------------------------------- */
+html {
+    position: relative;
+    min-height: 100%;
+}
+
+body {
+    /* Margin bottom by footer height */
+    margin-bottom: 60px;
+}
+
+.footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    white-space: nowrap;
+    line-height: 60px; /* Vertically center the text there */
+}
+
+/* Contatos */
+.contact-form {
+    background: linear-gradient(left, #0072ff, #00c6ff);
+    margin-top: 10%;
+    margin-bottom: 5%;
+    width: 70%;
+}
+
+    .contact-form .form-control {
+        border-radius: 1rem;
+    }
+
+.contact-image {
+    text-align: center;
+}
+
+    .contact-image img {
+        border-radius: 6rem;
+        width: 11%;
+        margin-top: -3%;
+        transform: rotate(29deg);
+    }
+
+.contact-form form {
+    padding: 14%;
+}
+
+    .contact-form form .row {
+        margin-bottom: -7%;
+    }
+
+.contact-form h3 {
+    margin-bottom: 8%;
+    margin-top: -10%;
+    text-align: center;
+    color: #0062cc;
+}
+
+.contact-form .btn {
+    width: 100%;
+    border: none;
+    border-radius: 1rem;
+    padding: 1.5%;
+    background: #0575e6;
+    font-weight: 600;
+    color: #fff;
+    cursor: pointer;
+}
+
+input.btn[type="submit"]:hover {
+    background: #00f260;
+    color: #0575e6;
+}
+
+.btn {    
+    border-radius: 1rem;
+    padding: 1.5%;
+    color: #fff;
+    background-color: #0062cc;
+    border: none;
+    cursor: pointer;
+    font-weight:bold;
+}
+
+.logo {
+    width: 100px;
+    height: 100px;
+}
+
+.color {
+    background: linear-gradient(to right, #00f260, #0575e6);
+    font-size: 20px;
+    font-weight: 800;
+    align-items: center;
+    text-align: center;
+    color: #fff;
+    text-decoration-color: #fff;
+}
+
+.color2 {
+    background: linear-gradient(to right, #283c86, #45a247);
+    color: #fff;
+    border-radius: 50%;
+    padding: 5px;
+    font-size: 20px;
+    font-weight: 800;
+    align-items: center;
+    text-align: center;
+}
+
+header {
+    padding-bottom:10px;
+}
